0bcf68ac7f95cd9a8e9dd48e59f834823adf7a7c
mrwan mohamed <phonekymy166@example.com>
2025-08-29 18:08:17 +0300

    4 commit

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,11 +1,24 @@
-import { NextResponse } from "next/server";
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
 
 export async function POST(req) {
-  const body = await req.json();
-  console.log("📩 Webhook body:", body);
-  return NextResponse.json({ received: true });
-}
+  try {
+    const evt = await verifyWebhook({
+      req,
+      secret: process.env.CLERK_WEBHOOK_SECRET, // استخدم السيكرت من env
+    });
 
-export async function GET() {
-  return NextResponse.json({ message: "Webhook route working (GET)" });
+    // Print webhook info
+    const { id } = evt.data;
+    const eventType = evt.type;
+
+    console.log(`✅ Received webhook with ID: ${id}`);
+    console.log(`📌 Event type: ${eventType}`);
+    console.log("🔍 Full payload:", JSON.stringify(evt.data, null, 2));
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("❌ Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
+  }
 }
